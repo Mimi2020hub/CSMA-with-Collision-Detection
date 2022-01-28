5c5981816236b78fa4e96327d3971574112226b8
Mimi <63885221+Mimi2020hub@users.noreply.github.com>
2022-01-28 10:05:25 -0500

    Add files via upload

---
+++ p2q4.py
@@ -0,0 +1,239 @@
+# CSMA/CD Algorithm
+import random
+import math
+import collections
+import matplotlib.pyplot as plt
+import numpy as np
+
+maxSimulationTime = 100
+total_num = 0
+class Node:
+    def __init__(self, location, A, is_exponential_backoff_time):
+        self.queue = collections.deque(self.generate_queue(A))
+        self.location = location  # Defined as a multiple of D
+        self.collisions = 0
+        self.wait_collisions = 0
+        self.MAX_COLLISIONS = 10 if is_exponential_backoff_time == True else 15
+
+    def collision_occured(self, R, is_exponential_backoff_time):
+        self.collisions += 1
+        if self.collisions > self.MAX_COLLISIONS:
+            # Drop packet and reset collisions
+            return self.pop_packet()
+
+        if is_exponential_backoff_time == True:
+            # Add the exponential backoff time to waiting time
+            backoff_time = self.queue[0] + self.exponential_backoff_time(R, self.collisions)
+        else:
+            backoff_time = self.queue[0] + self.contention_window_backoff_time(self.collisions)
+
+            for i in range(len(self.queue)):
+                if backoff_time >= self.queue[i]:
+                    self.queue[i] = backoff_time
+                else:
+                    break
+
+    def contention_window_backoff_time(self, collisions):
+        if collisions < 10:
+            k = collisions
+        else:
+            k = 10
+        return random.randint(1,pow(2,collisions))*51.2*pow(10,-6)                                      
+                    
+    def successful_transmission(self):
+        self.collisions = 0
+        self.wait_collisions = 0
+
+    def generate_queue(self, A):
+        packets = []
+        arrival_time_sum = 0
+
+        while arrival_time_sum <= maxSimulationTime:
+            arrival_time_sum += get_exponential_random_variable(A)
+            packets.append(arrival_time_sum)
+        return sorted(packets)
+
+    def exponential_backoff_time(self, R, general_collisions):
+        rand_num = random.random() * (pow(2, general_collisions) - 1)
+        return rand_num * 512/float(R)  # 512 bit-times
+
+    def pop_packet(self):
+        self.queue.popleft()
+        self.collisions = 0
+        self.wait_collisions = 0
+
+    def non_persistent_bus_busy(self, R):
+        self.wait_collisions += 1
+        if self.wait_collisions > self.MAX_COLLISIONS:
+            # Drop packet and reset collisions
+            return self.pop_packet()
+
+        # Add the exponential backoff time to waiting time
+        backoff_time = self.queue[0] + self.exponential_backoff_time(R, self.wait_collisions)
+
+        for i in range(len(self.queue)):
+            if backoff_time >= self.queue[i]:
+                self.queue[i] = backoff_time
+            else:
+                break
+
+
+def get_exponential_random_variable(param):
+    # Get random value between 0 (exclusive) and 1 (inclusive)
+    uniform_random_value = 1 - random.uniform(0, 1)
+    exponential_random_value = (-math.log(1 - uniform_random_value) / float(param))
+
+    return exponential_random_value
+
+def build_nodes(N, A, D, is_exponential_backoff_time):
+    nodes = []
+    for i in range(0, N):
+        nodes.append(Node(i*D, A, is_exponential_backoff_time))
+    return nodes
+
+def csma_cd(N, A, R, L, D, S, is_persistent, is_exponential_backoff_time):
+    curr_time = 0
+    transmitted_packets = 0
+    successfuly_transmitted_packets = 0
+    nodes = build_nodes(N, A, D, is_exponential_backoff_time)
+
+    while True:
+
+    # Step 1: Pick the smallest time out of all the nodes
+        min_node = Node(None, A, is_exponential_backoff_time)  # Some random temporary node
+        min_node.queue = [float("infinity")]
+        for node in nodes:
+            if len(node.queue) > 0:
+                min_node = min_node if min_node.queue[0] < node.queue[0] else node
+
+        if min_node.location is None:  # Terminate if no more packets to be delivered
+            break
+
+        curr_time = min_node.queue[0]
+        transmitted_packets += 1
+
+        # Step 2: Check if collision will happen
+        # Check if all other nodes except the min node will collide
+        collsion_occurred_once = False
+        for node in nodes:
+            if node.location != min_node.location and len(node.queue) > 0:
+                delta_location = abs(min_node.location - node.location)
+                t_prop = delta_location / float(S)
+                t_trans = L/float(R)
+
+                # Check collision
+                will_collide = True if node.queue[0] <= (curr_time + t_prop) else False
+
+                # Sense bus busy
+                if (curr_time + t_prop) < node.queue[0] < (curr_time + t_prop + t_trans):
+                    if is_persistent is True:
+                        for i in range(len(node.queue)):
+                            if (curr_time + t_prop) < node.queue[i] < (curr_time + t_prop + t_trans):
+                                node.queue[i] = (curr_time + t_prop + t_trans)
+                            else:
+                                break
+                    else:
+                        node.non_persistent_bus_busy(R)
+
+                if will_collide:
+                    collsion_occurred_once = True
+                    transmitted_packets += 1
+                    node.collision_occured(R, is_exponential_backoff_time)
+
+        # Step 3: If a collision occured then retry
+        # otherwise update all nodes latest packet arrival times and proceed to the next packet
+        if collsion_occurred_once is not True:  # If no collision happened
+            successfuly_transmitted_packets += 1
+            min_node.pop_packet()
+        else:    # If a collision occurred
+            min_node.collision_occured(R, is_exponential_backoff_time)
+
+    Effeciency = successfuly_transmitted_packets/float(transmitted_packets)
+    Throughput = (L * successfuly_transmitted_packets) / float(curr_time + (L/R)) * pow(10, -6)
+    
+    return Effeciency, Throughput
+
+# Run Algorithm
+# N = The number of nodes/computers connected to the LAN
+# A = Average packet arrival rate (packets per second)
+# R = The speed of the LAN/channel/bus (in bps)
+# L = Packet length (in bits)
+# D = Distance between adjacent nodes on the bus/channel
+# S = Propagation speed (meters/sec)
+
+D = 10
+C = 3 * pow(10, 8) # speed of light
+S = (2/float(3)) * C
+R = 1 * pow(10, 6)
+L = 1500
+Persistent = ["Non persistent", "Persistent"]
+Original = [" Revised", " Original"]
+
+state_list = []
+y_lable=[]
+for i in range(2):
+    for j in range(2):
+        state_list.append([i,j])
+        y_lable.append(Persistent[i]+Original[j])
+
+A = 10
+x = np.linspace(20,100,5)
+y_Efficiency = [[],[],[],[]]
+y_Throughput = [[],[],[],[]]
+for k in range(4):
+    for N in range(20, 101, 20):
+        E, T = csma_cd(N, A, R, L, D, S, state_list[k][0], state_list[k][1])
+        y_Efficiency[k].append(E)
+        y_Throughput[k].append(T)
+
+fig1a, N_Efficiency = plt.subplots()
+for k in range(4):
+    N_Efficiency.plot(x, y_Efficiency[k], label = y_lable[k])
+N_Efficiency.set_xlabel("Number of Users")
+N_Efficiency.set_ylabel("Efficiency")
+N_Efficiency.set_title("Figure_1_a")
+N_Efficiency.legend()
+plt.savefig("Figure_1_a.png")
+plt.clf()
+
+fig1b, N_Throughput = plt.subplots()
+for k in range(4):
+    N_Throughput.plot(x, y_Throughput[k], label = y_lable[k])
+N_Throughput.set_xlabel("Number of Users")
+N_Throughput.set_ylabel("Throughput")
+N_Throughput.set_title("Figure_1_b")
+N_Throughput.legend()
+plt.savefig("Figure_1_b.png")
+plt.clf()
+
+
+N = 20
+x = np.linspace(5,20,4)
+y_Efficiency = [[],[],[],[]]
+y_Throughput = [[],[],[],[]]
+for k in range(4):
+    for A in range(5, 21, 5):
+        E, T = csma_cd(N, A, R, L, D, S, state_list[k][0], state_list[k][1])
+        y_Efficiency[k].append(E)
+        y_Throughput[k].append(T)
+        
+fig2a, N_Efficiency = plt.subplots()
+for k in range(4):
+    N_Efficiency.plot(x, y_Efficiency[k], label = y_lable[k])
+N_Efficiency.set_xlabel("Packet Delivery Rate")
+N_Efficiency.set_ylabel("Efficiency")
+N_Efficiency.set_title("Figure_2_a")
+N_Efficiency.legend()
+plt.savefig("Figure_2_a.png")
+plt.clf()
+
+fig2b, N_Throughput = plt.subplots()
+for k in range(4):
+    N_Throughput.plot(x, y_Throughput[k], label = y_lable[k])
+N_Throughput.set_xlabel("Packet Delivery Rate")
+N_Throughput.set_ylabel("Throughput")
+N_Throughput.set_title("Figure_2_b")
+N_Throughput.legend()
+plt.savefig("Figure_2_b.png")
+plt.clf()        
+
